d85bdcda56965008510795d6e16c231cb1a0498c
tosh1ue <tosh1ue@qq.com>
2026-03-07 00:00:01 +0800

    init commit

--- example.c
+++ example.c
@@ -1,16 +1,24 @@
 #include "draw_scramble/draw_scramble.h"
 #include <stdio.h>
+#include <stdint.h>
+
+uint8_t nums[3] = {1, 2, 3};
+
+void test_print(uint8_t* nums) {
+  printf("%d %d %d\n", nums[0], nums[1], nums[2]);
+}
 
 void main(void)  {
 
   // F L R B
-  printf("+-----------+\n");
-  printf("| %d | %d | %d |\n", 3, 3, 3);
-  printf("+-----------+\n");
-  printf("| %d | %d | %d |\n", 3, 3, 3);
-  printf("+-----------+\n");
-  printf("| %d | %d | %d |\n", 3, 3, 3);
-  printf("+-----------+\n");
+  // printf("+-----------+\n");
+  // printf("| %d | %d | %d |\n", 3, 3, 3);
+  // printf("+-----------+\n");
+  // printf("| %d | %d | %d |\n", 3, 3, 3);
+  // printf("+-----------+\n");
+  // printf("| %d | %d | %d |\n", 3, 3, 3);
+  // printf("+-----------+\n");
+  test_print(nums);
 
   while (1) {
 
