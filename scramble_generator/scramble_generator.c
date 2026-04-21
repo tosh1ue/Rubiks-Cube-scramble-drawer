3efc4e37f6164b940437d579d45d438caca3ef11
tosh1ue <tosh1ue@qq.com>
2026-04-21 11:17:17 +0800

    add scramble_generator module

---
+++ scramble_generator/scramble_generator.c
@@ -0,0 +1,8 @@
+/**
+ * @file scramble_generator.c
+ * @brief 生成WCA标准打乱公式
+ * @author tosh1ue
+ * @date 2026-04-21
+ */
+
+#include "scramble_generator.h"
---
+++ scramble_generator/scramble_generator.h
@@ -0,0 +1,21 @@
+/**
+ * @file scramble_generator.h
+ * @brief 生成WCA标准打乱公式
+ * @author tosh1ue
+ * @date 2026-04-21
+ */
+
+#ifndef SCRAMBLE_GENERATOR_H
+#define SCRAMBLE_GENERATOR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
